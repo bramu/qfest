f151b81b308d539830f2669563b29fc33626ed6f
Ali <syedali0111@gmail.com>
2012-02-23 17:13:52 +0530

    adding for check

--- src/com/tb/dao/QuestionDAO.java
+++ src/com/tb/dao/QuestionDAO.java
@@ -1,5 +1,114 @@
 package com.tb.dao;
 
-public class QuestionDAO {
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import com.tb.beans.*;
+import com.tb.utils.*;
 
+public class QuestionDAO {
+	private String tableName = "questions";
+	private String usertable = "userdata";
+	private Statement stm;
+	public QuestionDAO() {
+		try {
+			
+			Connection con = DBConnector.getInstance().getConnection();
+					
+			stm = con.createStatement();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public int getTotalCount() throws SQLException {
+	    ResultSet rs = stm.executeQuery("select count(*) as total_count from " + tableName);
+	    
+	    while (rs.next()) {
+	    	return rs.getInt(1);
+	    }
+	    return 0;
+	}
+	
+	public List<Question> fetchAll(int pageNo) throws SQLException {
+	    ResultSet rs = stm.executeQuery("select id, title from " + tableName + " limit " + (pageNo - 1) * 3 + ", 3" );
+	    List<Question> questions = new ArrayList<Question>();
+	    
+	    while (rs.next()) {
+	    	Question q = new Question();
+	    	q.setId(rs.getInt(1));
+	    	q.setTitle(rs.getString(2));
+	    	questions.add(q);
+	    }
+	    return questions;
+	}
+	
+	public Question findById(int questionId) throws SQLException {
+		ResultSet rs = stm.executeQuery("select id, title from " + tableName + " WHERE id = " + questionId);
+		Question q = new Question();
+	    while (rs.next()) {
+	    	q.setId(rs.getInt(1));
+	    	q.setTitle(rs.getString(2));
+	    }
+	    return q;
+	}
+	
+	
+	public Question create(String title) throws SQLException {
+		Question q = new Question();
+		int id = stm.executeUpdate("insert into "+ tableName +"(title,company_id) values('"+ title +"','1')");
+		q.setId(id);
+		q.setTitle(title);
+		return q;
+	}
+	
+	public Question update(int id,String title) throws SQLException {
+		Question q = new Question();
+		int question_id = stm.executeUpdate("update "+ tableName +" set title = '"+ title + "' where id=" + id);
+		q.setId(id);
+		q.setTitle(title);
+		return q;
+	}
+	
+	public void delete( int id) throws SQLException {
+		int id1 = stm.executeUpdate("delete from "+ tableName +" where id=" + id);	
+	}
+	public User createAccount(String name,String email,String pw,String c_pw) throws SQLException{
+		User u = new User();
+		int id = stm.executeUpdate("insert into " + usertable +"(name,email,password,confirm_pw) values" +
+				"('"+ name +"','"+ email +"','"+ pw +"','"+ c_pw +"') ");		
+		u.setId(id);
+		u.setName(name);
+		u.setEmail(email);
+		u.setPassword(pw);
+		u.setConfirm_pw(c_pw);
+		return u;
+	}
+	public User loginCheck(String email, String password) throws SQLException{
+		ResultSet rs = stm.executeQuery("select email, password from " + usertable + " WHERE email = '"
+	                                  + email + "' and password = '" + password + "'");
+		User u = null;
+	    
+	    while (rs.next()) {
+	    	u = new User();
+	    	u.setEmail(rs.getString(1));
+	    	u.setPassword(rs.getString(2));
+	    }
+	    return u;
+	}
+	public List<Question> paging() throws SQLException {
+	    ResultSet rs = stm.executeQuery("select id, title from " + tableName +"limit 11,10");
+	    List<Question> questions = new ArrayList<Question>();
+	    
+	    while (rs.next()) {
+	    	Question q = new Question();
+	    	q.setId(rs.getInt(1));
+	    	q.setTitle(rs.getString(2));
+	    	questions.add(q);
+	    }
+	    return questions;
+	}
 }
--- src/com/tb/servlets/QuestionsServlet.java
+++ src/com/tb/servlets/QuestionsServlet.java
@@ -1,7 +1,205 @@
 package com.tb.servlets;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.tb.beans.*;
+import com.tb.dao.*;
 
 public class QuestionsServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private QuestionDAO dao = new QuestionDAO();
 
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		try {
+			performAction(req, resp);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		try {
+			performAction(req, resp);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public void performAction(HttpServletRequest req, HttpServletResponse resp)
+			throws SQLException, IOException {
+		if (req.getParameter("action") == null) {
+			performIndexAction(req, resp);
+		} else if (req.getParameter("action").equals("index")) {
+			performIndexAction(req, resp);
+		} else if (req.getParameter("action").equals("new")) {
+			performNewAction(req, resp);
+		} else if (req.getParameter("action").equals("create")) {
+
+			performCreateAction(req, resp);
+		} else if (req.getParameter("action").equals("view")) {
+
+			performViewAction(req, resp);
+		} else if (req.getParameter("action").equals("edit")) {
+			performEditAction(req, resp);
+		} else if (req.getParameter("action").equals("update")) {
+
+			performUpdateAction(req, resp);
+		} else if (req.getParameter("action").equals("delete")) {
+
+			performDeleteAction(req, resp);
+		} else if (req.getParameter("action").equals("signout")) {
+
+			performSignoutAction(req, resp);
+		}
+			
+	}
+
+	private void performIndexAction(HttpServletRequest req,
+			HttpServletResponse resp) {
+		try {
+			int pageNo = 1;
+			if (req.getParameter("page") != null) {
+				pageNo = Integer.parseInt(req.getParameter("page"));
+			}
+			List<Question> questions = dao.fetchAll(pageNo);
+			req.setAttribute("questions", questions);
+			req.setAttribute("totalCount", dao.getTotalCount());
+			RequestDispatcher rd = getServletContext().getRequestDispatcher("/questions/index.jsp");
+			rd.forward(req, resp);
+
+			/*if (req.getSession().getAttribute("is_logged_in") == null
+					|| (Boolean) req.getSession().getAttribute("is_logged_in") == false) {
+				pw.write("<a href = '/w5db/users'>AddQuestion</a>");
+			} else {
+				pw.write("You are successfully logged in");
+				pw.write("<a href = '/w5db/questions?action=new'>AddQuestion</a> <br/>");
+			}
+			
+			/*for (Question question : questions) {
+				pw.write(question.getTitle());
+				req.setAttribute("questions", questions);
+				RequestDispatcher rd = getServletContext().getRequestDispatcher("/questions/index.jsp");
+				rd.forward(req, resp);
+					
+			}
+			pw.write("<a href = '/w5db/questions?action=signout'>signout</a> <br/>");*/
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private void performNewAction(HttpServletRequest req,
+			HttpServletResponse resp) {
+		try {
+			
+			RequestDispatcher rd = getServletContext().getRequestDispatcher("/questions/new.jsp");
+			rd.forward(req, resp);
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private void performCreateAction(HttpServletRequest req,
+			HttpServletResponse resp) {
+		try {
+			
+			resp.setContentType("text/html");
+			dao.create(req.getParameter("title"));
+			resp.sendRedirect("/qfest/questions?action=index");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void performViewAction(HttpServletRequest req,
+			HttpServletResponse resp) {
+		try {
+			Question q = dao.findById(Integer.parseInt(req.getParameter("id")));
+			req.setAttribute("question", q);
+			RequestDispatcher rd  = getServletContext().getRequestDispatcher("/questions/view.jsp");
+			rd.forward(req, resp);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void performEditAction(HttpServletRequest req,
+			HttpServletResponse resp) {
+		try {
+			Question q = new Question();
+			q = dao.findById(Integer.parseInt(req.getParameter("id")));
+			req.setAttribute("question", q);
+			RequestDispatcher rd = getServletContext().getRequestDispatcher("/questions/edit.jsp");
+			rd.forward(req, resp);
+			
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private void performUpdateAction(HttpServletRequest req,
+			HttpServletResponse resp) {
+		try {
+			resp.setContentType("text/html");
+			Question q = new Question();
+			q = dao.update(Integer.parseInt(req.getParameter("id")),
+					req.getParameter("title"));
+			resp.sendRedirect("/qfest/questions?action=index");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private void performDeleteAction(HttpServletRequest req,
+			HttpServletResponse resp) {
+		try {
+
+			resp.setContentType("text/html");
+			dao.delete(Integer.parseInt(req.getParameter("id")));
+			resp.sendRedirect("/qfest/questions?action=index");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private void performSignoutAction(HttpServletRequest req,
+			HttpServletResponse resp) {
+		resp.setContentType("text/html");
+		try {
+			PrintWriter pw = resp.getWriter();
+			if ((Boolean) req.getSession().getAttribute("is_logged_in") == true) {
+				req.getSession().setAttribute("is_logged_in", false);
+				resp.sendRedirect("/qfest/questions");
+			}
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
 }
